fe7a41e1c59eeb1a842652dfe18fcb745c4fc336
İSMAİL TAŞDELEN <pentestdatabase@gmail.com>
2016-04-17 19:35:28 +0300

    Added files via upload

---
+++ portscanner.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from socket import *
+
+star = '*********************************************************************'
+print star
+portscanner_ico = '''
+#########################################################
+#              PORT SCANNER - GH0ST S0FTWARE            #
+######################################################### 
+#                       CONTACT                         #
+#########################################################
+#              DEVELOPER : İSMAİL TAŞDELEN              #                       
+#        Mail Address : pentestdatabase@gmail.com       #
+# LINKEDIN : https://www.linkedin.com/in/ismailtasdelen #
+#           Whatsapp : + 90 534 295 94 31               #
+#########################################################
+'''
+print portscanner_ico
+print star
+if __name__ == '__main__':
+    target = raw_input('Enter Host To Scan ---> ')
+    targetIP = gethostbyname(target)
+    print star
+    print 'Starting scan on host... ', targetIP
+    print star
+
+    for i in range(0, 65535):
+        s = socket(AF_INET, SOCK_STREAM)
+
+        result = s.connect_ex((targetIP, i))
+
+        if(result == 0) :
+            print 'PORT %d: AÇIK' % (i,)
+        s.close()
